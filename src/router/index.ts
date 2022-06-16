0e3682e4892776d1e9f6fa966233eae429d0df7d
MauroMontan <mauroanastacio@outlook.es>
2022-06-15 19:36:00 -0500

    adding main layout

---
+++ src/router/index.ts
@@ -0,0 +1,14 @@
+
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
+const routes: Array<RouteRecordRaw> = [
+  { path: '/home', component: () => import("../pages/home_page.vue") },
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes, // short for `routes: routes`
+})
+
+export default router;
+
+
